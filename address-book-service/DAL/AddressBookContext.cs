0864efa4fca76f1e123168bc95265119a94bb11a
dejan111 <dejan.pavkovic@gmail.com>
2020-07-11 15:53:12 +0200

    ADD preparation for migration and DB create

---
+++ address-book-service/DAL/AddressBookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class AddressBookContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options) { }
+    }
+}
